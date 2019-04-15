4cf1090d69399e12a52f75d3c636b51ad31874df
BintangSaputra <bintangcbs26@gmail.com>
2019-04-15 15:57:12 +0700

    improve index js

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-let kiloGram = prompt("weight");
-let height = prompt("height");
-let age = prompt("age");
+const resultBmr = () => {
+  let kiloGram = document.getElementById("weight").value;
+  let height = document.getElementById("height").value;
+  let age = document.getElementById("age").value;
+  console.log(kiloGram);
 
-const resultBmr = 88.362 + 13.397 * kiloGram + 4.799 * height - 5.677 * age;
+  const valueBmr = 88.362 + 13.397 * kiloGram + 4.799 * height - 5.677 * age;
+  return (document.getElementById("result").value = Math.round(valueBmr));
+};
 
-console.log(Math.round(resultBmr));
+// console.log(Math.round(resultBmr(kiloGram, height, age)));
 
-const notActive = resultBmr * 1.2;
-const lightlyActive = resultBmr * 1.375;
-const active = resultBmr * 1.55;
-const veryActive = resultBmr * 1.725;
+// const notActive = resultBmr * 1.2;
+// const lightlyActive = resultBmr * 1.375;
+// const active = resultBmr * 1.55;
+// const veryActive = resultBmr * 1.725;
 
-const activity = prompt(`How active are you?
-1: not active
-2: lightly active
-3: active
-4: very active
-`);
+// const activity = `How active are you?
+// 1: not active
+// 2: lightly active
+// 3: active
+// 4: very active
+// `;
 
-if (activity == 1) {
-  alert(notActive);
-} else if (activity == 2) {
-  alert(lightlyActive);
-} else if (activity == 3) {
-  alert(active);
-} else {
-  alert(veryActive);
-}
+// if (activity == 1) {
+//   alert(notActive);
+// } else if (activity == 2) {
+//   alert(lightlyActive);
+// } else if (activity == 3) {
+//   alert(active);
+// } else {
+//   alert(veryActive);
+// }
 
-console.log(Math.round(activity));
+// console.log(Math.round(activity));
